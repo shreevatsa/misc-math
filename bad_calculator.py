196e951e2d1d99bb3e18950512f7a51d2ebb874b
Shreevatsa R <shreevatsa@google.com>
2012-03-26 12:07:49 +0530

    temporary hack in bad_calculator: not fixed Don't use this version; go back to older version and use it.

--- bad_calculator.py
+++ bad_calculator.py
@@ -45,7 +45,7 @@ class Bin:
     thus the string '0b1'+s represents
         v * 2**(len(s)-e)
     '''
-    
+
     def __init__(self, s, e, k=16):
         t = s.ljust(k, '0')[:k]
         assert len(t) == k
@@ -59,3 +59,66 @@ class Bin:
     def multiply(self, b):
         return multiply(self, b)
 
+def next(s):
+  '''The next binary string after s'''
+  return bin(eval(s) + 1)
+
+def prev(s):
+  return bin(eval(s) - 1)
+
+def mystr(n):
+  return '%.50f' % n
+
+# TODO
+def smaller(s, t):
+  '''Whether when read in decimal, string s is smaller than string t'''
+  return s < t
+
+# TODO
+def represent(f):
+  '''The (s,e) for a given decimal float'''
+
+  # TODO: is garbage when f < 1
+  floor = f
+  if f.find('.') != -1:
+    floor = f[:f.find('.')]
+  exp = len(bin(int(floor))) - 3
+
+  lower_cur = '1'
+  upper_cur = '10'
+  # TODO: change to yield
+  for dummy in range(100):
+    exp -= 1
+    s0 = '0b' + lower_cur + '0'
+    s1 = next(s0)
+    s2 = next(s1)
+
+    t0 = '0b' + upper_cur + '0'
+    t1 = prev(t0)
+    t2 = prev(t1)
+
+    # print 'Checking: '
+    # print '\t Lower %s = %.50f' % (s0, eval(s0)*(2**exp))
+    # print '\t Lower %s = %.50f' % (s1, eval(s1)*(2**exp))
+    # print '\t Lower %s = %.50f' % (s2, eval(s2)*(2**exp))
+    # print '\t Upper %s = %.50f' % (t0, eval(t0)*(2**exp))
+    # print '\t Upper %s = %.50f' % (t1, eval(t1)*(2**exp))
+    # print '\t Upper %s = %.50f' % (t2, eval(t2)*(2**exp))
+
+    assert smaller(mystr(eval(s0)*(2**(exp))), f)
+    if smaller(mystr(eval(s1)*(2**(exp))), f):
+      lower_cur = s1[2:]
+    else:
+      lower_cur = s0[2:]
+
+    assert smaller(f, mystr(eval(t0)*(2**exp)))
+    if smaller(f, mystr(eval(t1)*(2**exp))):
+      upper_cur = t1[2:]
+    else:
+      upper_cur = t0[2:]
+
+    print lower_cur, upper_cur, exp
+
+represent('348.99')
+# represent('348.99000000000000909494701772928237915039062500000000')
+# '0b101011100111111010111000010100011110101110000101000111 * 
