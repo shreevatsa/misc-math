6a24b0cc5a86097ed808c3e83add32d749863919
Shreevatsa R <shreevatsa@google.com>
2014-04-14 17:35:42 +0530

    MSE-736840

---
+++ sums.cc
@@ -0,0 +1,49 @@
+// Given n, S, k, find n1 to nk, adding up to n, whose squares add up to k.
+// (Brute force.)
+
+#include <vector>
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+int N, S, K;
+vector<int> a;
+int csum, ssum;
+
+void search(int i) {
+  assert(a.size() == i);
+  if (i == 0) {
+    csum = 0;
+    ssum = 0;
+  }
+  if (i == K - 1) {
+    int last = N - csum;
+    if (last < a[i - 1] or ssum + last * last != S) return;
+    a.push_back(last); csum += last; ssum += last * last;
+    search(i + 1);
+    a.pop_back(); csum -= last; ssum -= last * last;
+    return;
+  }
+  if (i == K) {
+    if (csum != N) return;
+    if (ssum != S) return;
+    for (int j = 0; j < i; ++j) cout << a[j] << " ";
+    cout << endl;
+    return;
+  }
+  int last = 1;
+  if (i > 0) last = a[i - 1];
+  for (int c = last; csum + (K - i) * c <= N and ssum + (K-i) * c*c <= S; ++c) {
+    a.push_back(c); csum += c; ssum += c * c;
+    search(i + 1);
+    a.pop_back(); csum -= c; ssum -= c * c;
+  }
+}
+
+int main() {
+  cin >> N >> S;
+  for (K = 2; K <= N; ++K) {
+    cout << endl << "K = " << K << endl;
+    search(0);
+  }
+}
