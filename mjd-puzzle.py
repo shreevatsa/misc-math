509e8a7983bde6c99cff4e6d5974bd8173d96fa8
Shreevatsa R <shreevatsa.public@gmail.com>
2016-07-13 12:53:39 -0700

    Also keep expressions. And fixed bug: - and / are not commutative and we need both op(a, b) and op(b, a)

--- mjd-puzzle.py
+++ mjd-puzzle.py
@@ -1,35 +1,51 @@
 import operator
 
+# Expression: (value, string)
+def value(p): return p[0]
+def expr(p): return p[1]
+
+def symbol(op):
+    return {
+        operator.add: '+',
+        operator.sub: '-',
+        operator.mul: '*',
+        operator.truediv: '/'
+        }[op]
+
 def iterate(poss):
   newposs = set()
   for l in poss:
     for a in range(len(l)):
-        for b in range(a + 1, len(l)):
+        for b in range(len(l)):
+            if b == a: continue
             for op in [operator.add, operator.sub, operator.mul, operator.truediv]:
-                if op == operator.truediv and l[b] == 0: continue
-                v = op(l[a], l[b])
-                if v == 17: print op, l[a], l[b]
-                nl = [v]
+                if op == operator.truediv and value(l[b]) == 0: continue
+                v = op(value(l[a]), value(l[b]))
+                s = '(%s %s %s)' % (expr(l[a]), symbol(op), expr(l[b]))
+                nl = [(v, s)]
                 for x in range(len(l)):
                     if x not in [a, b]:
                         nl.append(l[x])
                 newposs.add(tuple(sorted(nl)))
   return newposs
 
-t = (2, 5, 6, 6)
+t = ((2, '2'), (5, '5'), (6, '6'), (6, '6'))
 poss = set([t])
 
+def p():
+    print '\n'.join(str(x) for x in list(sorted(poss)))
+
 print 'Start'
-print list(sorted(poss))
+p()
 
 print 'One'
 poss = iterate(poss)
-print list(sorted(poss))
+p()
 
 print 'two'
 poss = iterate(poss)
-print list(sorted(poss))
+p()
 
 print 'three'
 poss = iterate(poss)
-print list(sorted(poss))
+p()
