0b15b18805b40ca4181c1c213c350375eda3b6e2
Shreevatsa R <shreevatsa.public@gmail.com>
2016-07-17 07:30:51 -0700

    Actually negative values don't make sense on left either: -b + a can be written a - b. But we may need them in case of -b - a?
    Just trying to avoid dupes like this:
    
    26/5=((2 - 6) / 5) + 6
    26/5=6 - ((6 - 2) / 5)

--- mjd-puzzle-2.py
+++ mjd-puzzle-2.py
@@ -744,9 +744,9 @@ def iterate(poss):
             for (candidates_l, candidates_r, others) in three_subsets(l):
                 if not candidates_l: continue
                 if len(candidates_l) == 1 and len(candidates_r) == 0: continue
-                # To avoid dupes: we avoid negative / small values on the right.
-                if (operation == ADD_SUB and any(e.poss_negation and e.value < 0 for e in candidates_r) or
-                    operation == MUL_DIV and any(e.poss_reciprocal and e.value < 1 for e in candidates_l)):
+                # To avoid dupes: we avoid negative / small values on the right *or* left
+                if (operation == ADD_SUB and any(e.poss_negation and e.value < 0 for e in candidates_r + candidates_l) or
+                    operation == MUL_DIV and any(e.poss_reciprocal and e.value < 1 for e in candidates_r + candidates_l)):
                     continue
                 # Avoid dividing by zero
                 if operation == MUL_DIV and any(e.value == 0 for e in candidates_r):
